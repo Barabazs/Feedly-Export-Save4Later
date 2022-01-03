09622360e7dc364e02f3ad39e3257a07a64655d5
Barabazs <31799121+Barabazs@users.noreply.github.com>
2022-01-03 20:38:39 +0100

    Update selectors for latest Feedly changes h/t @mattgortner

--- SaveAllEntries.js
+++ SaveAllEntries.js
@@ -1,31 +1,31 @@
 let convertEntries = () => {
-    "use strict";
-    let target = [...document.getElementsByClassName("entry")];
-    let result = [];
-    target.forEach((element) => {
-        let regExpLiteral = /Published:.(.*)/i;
-        result.push({
-            title: element.getAttribute("data-title"),
-            url: element.getAttribute("data-alternate-link"),
-            summary: element.querySelectorAll(".summary")[0].innerHTML,
-            time: regExpLiteral.exec(element.querySelectorAll("span.ago")[0].title)[1],
-            sourceTitle: element.querySelectorAll(".entry__source")[0].innerHTML,
-            sourceUrl: element.querySelectorAll(".entry__source")[0].href,
-        });
+  "use strict";
+  let target = [...document.getElementsByClassName("entry")];
+  let result = [];
+  target.forEach((element) => {
+    let regExpLiteral = /Published:.(.*)/i;
+    result.push({
+      title: element.querySelector(".entry__title").innerHTML,
+      url: element.getAttribute("data-alternate-link"),
+      summary: element.querySelector(".summary").innerHTML,
+      time: regExpLiteral.exec(element.querySelector("span.ago").title)[1],
+      sourceTitle: element.querySelector(".entry__source").innerHTML,
+      sourceUrl: element.querySelector(".entry__source").href,
     });
-    return result;
+  });
+  return result;
 };
 
 let saveToFile = (input) => {
-    "use strict";
-    let json = JSON.stringify(input, undefined, 2);
-    let blob = new Blob([json], { type: "text/plain;charset=utf-8" });
-    let downloadLink = document.createElement("a");
-    downloadLink.href = URL.createObjectURL(blob);
-    let fileName = "FeedlySavedForLater" + Date.now().toString() + ".json";
-    downloadLink.download = fileName;
-    downloadLink.click();
-    URL.revokeObjectURL(downloadLink.href);
+  "use strict";
+  let json = JSON.stringify(input, undefined, 2);
+  let blob = new Blob([json], { type: "text/plain;charset=utf-8" });
+  let downloadLink = document.createElement("a");
+  downloadLink.href = URL.createObjectURL(blob);
+  let fileName = "FeedlySavedForLater" + Date.now().toString() + ".json";
+  downloadLink.download = fileName;
+  downloadLink.click();
+  URL.revokeObjectURL(blob);
 };
 
 saveToFile(convertEntries());
