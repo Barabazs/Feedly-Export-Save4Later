62765c2fe977f6d974f0e393a6e154f1fb89fa78
Barabazs <31799121+Barabazs@users.noreply.github.com>
2023-02-03 08:49:57 +0100

    fix: saved entry selectors

--- SaveSavedEntries.js
+++ SaveSavedEntries.js
@@ -1,16 +1,16 @@
 let convertEntries = () => {
   "use strict";
-  let target = [...document.getElementsByClassName("quicklisted")];
+  let target = [...document.getElementsByClassName("list-entries")];
   let result = [];
   target.forEach((element) => {
     let regExpLiteral = /Published:.(.*)/i;
     result.push({
       title: element.querySelector(".EntryTitle").innerText,
-      url: element.querySelector("a:not(.entry__source)").href,
+      url: element.querySelector("a:not(.EntryMetadataSource)").href,
       summary: element.querySelector(".EntrySummary").innerText,
       time: regExpLiteral.exec(element.querySelector("span.ago").title)[1],
-      sourceTitle: element.querySelector(".entry__source").innerText,
-      sourceUrl: element.querySelector(".entry__source").href,
+      sourceTitle: element.querySelector(".EntryMetadataSource").innerText,
+      sourceUrl: element.querySelector(".EntryMetadataSource").href,
     });
   });
   return result;
